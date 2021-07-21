dc0c356671709f23ae62b979e908b1b61c8de999
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2021-07-21 11:52:20 -0500

    Create deletion-at-position.cpp

---
+++ linkedList/singlyLinkedList/deletion-at-position.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+
+void push(Node **head,Node **temp,int data)
+{
+    Node *newNode;
+    newNode= new Node();
+
+    if((*head)==NULL)
+        {
+             newNode->data=data;
+             newNode->next=(*head);
+             (*head)=(*temp)=newNode;
+        }
+
+    else{
+               newNode->data=data;
+               newNode->next=NULL;
+               (*temp)->next=newNode;
+               (*temp)=newNode;
+         }
+}
+
+void display(Node *head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<" -> ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+
+void deleteAtPosition(Node **head,int pos)
+{
+    Node *temp,*nextNode;
+    temp=(*head);
+    int i=1;
+    while(i<pos-1)
+    {
+        temp=temp->next;
+        i++;
+    }
+    nextNode=temp->next;
+    temp->next=nextNode->next;
+    delete(nextNode);
+}
+int main()
+{
+    Node *head=NULL,*temp=NULL;
+    int choice,data,pos;
+    do
+    {
+        cout<<"enter a data to pushed in link list : ";
+        cin>>data;
+        push(&head,&temp,data);
+        cout<<"press 1 for continue : ";
+        cin>>choice;
+    }while(choice==1);
+     display(head);
+
+     cout<<"enter position : ";
+     cin>>pos;
+     deleteAtPosition(&head,pos);
+     cout<<endl;
+     display(head);
+
+     return 0;
+}
