6f8bab6f95951b578bf6799fc4ccf3467323aa4e
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-02-23 12:58:36 +0530

    Create Detect-Loop.cpp

---
+++ linkedList/singlyLinkedList/Detect-Loop.cpp
@@ -0,0 +1,145 @@
+//check whether a Loop exists or not
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+Node *create(int d)
+{
+    Node *newNode = new Node();
+    newNode->data = d;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void print(Node *&head)
+{
+    if (head == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    cout << "data of linked list : ";
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void push(Node *&head, Node *&tail, int d)
+{
+    if (head == NULL)
+    {
+        head = create(d);
+        tail = head;
+    }
+    else
+    {
+        tail->next = create(d);
+        tail = tail->next;
+    }
+}
+
+// approach - 1 T.C = S.C = O(N)
+bool detectLoop(Node *head)
+{
+    if (head == NULL)
+        return false;
+
+    // using map
+    // unordered_map<Node *, bool> visit;
+    // Node *temp = head;
+    // while (temp != NULL)
+    // {
+    //     if (visit[temp] == true)
+    //         return true;
+
+    //     visit[temp] = true;
+    //     temp = temp->next;
+    // }
+    // return false;
+
+    //using set
+    unordered_set<Node *> s;
+    while (head != NULL)
+    {
+        if (s.find(head) != s.end())
+            return true;
+
+        s.insert(head);
+        head = head->next;
+    }
+    return false;
+
+}
+
+// approach - 2 T.C = O(N) and S.C = O(1)
+bool floydCycleDelect(Node *head)
+{
+    if (head == NULL)
+        return false;
+
+    Node *fast, *slow;
+    fast = slow = head;
+    while (fast != NULL && slow != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+            fast = fast->next;
+        
+        slow = slow->next;
+
+        if(fast == slow)
+           return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    Node *head, *tail;
+    head = tail = NULL;
+
+    // creating initial nodes in Linked List
+    int n;
+    cout << "enter the initial size of linked list : ";
+    cin >> n;
+    cout << endl
+         << "enter " << n << " numbers : ";
+    while (n--)
+    {
+        int d;
+        cin >> d;
+        push(head, tail, d);
+    }
+    print(head);
+    cout << "head data = " << head->data << " tail data =  " << tail->data << endl
+         << endl;
+
+    // for checking loop Approach 1
+    // tail->next = head->next;
+    // if (detectLoop(head))
+    // {
+    //     cout << "Linked List is circular in nature" << endl;
+    // }
+    // else
+    //     cout << "Linked List is not circular" << endl;
+
+    //for checking loop Approach 2
+    tail->next = head->next->next->next;
+    if (floydCycleDelect(head))
+        cout << "Linked List is circular in nature" << endl;
+    else
+        cout << "Linked List is not circular" << endl;
+}
