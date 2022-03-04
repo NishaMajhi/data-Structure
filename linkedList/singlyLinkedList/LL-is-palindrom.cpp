6e948ec90e9ec94b65f88c627257e31fc4a928de
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-03-04 20:41:56 +0530

    LL-is-palindrom

---
+++ linkedList/singlyLinkedList/LL-is-palindrom.cpp
@@ -0,0 +1,138 @@
+// find  Linked List  is palindrom or not
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+Node *create(int d)
+{
+    Node *newNode = new Node();
+    newNode->data = d;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void print(Node *&head)
+{
+    cout << "data of linked list : ";
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void push(Node *&head, Node *&tail, int d)
+{
+    if (head == NULL)
+    {
+        head = create(d);
+        tail = head;
+    }
+    else
+    {
+        tail->next = create(d);
+        tail = tail->next;
+    }
+}
+
+Node *reverse(Node *head)
+{
+    Node *next = NULL;
+    Node *curr = head;
+    Node *prev = NULL;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
+
+Node *getMid(Node *head)
+{
+    Node *fast = head->next;
+    Node *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+bool palindrom(Node *head)
+{ 
+    if(head->next == NULL)
+       return true;
+
+    Node *mid = getMid(head);
+
+    Node *temp = mid->next;
+    mid->next = reverse(temp);
+
+    Node *head1 = head;
+    Node *head2 = mid->next;
+    while (head2!= NULL)
+    {
+        if (head1->data != head2->data){
+            return 0;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+
+    temp = mid->next;
+    mid->next = reverse(temp);
+
+    return true;
+}
+
+int main()
+{
+    Node *head1, *tail1;
+    head1 = tail1 = NULL;
+
+    // creating initial nodes in Linked List
+    int n;
+    cout << "enter the size of first linked list : ";
+    cin >> n;
+    cout << endl
+         << "enter " << n << " numbers : ";
+    while (n--)
+    {
+        int d;
+        cin >> d;
+        push(head1, tail1, d);
+    }
+
+    if (palindrom(head1) == true)
+        cout << "palindrom";
+    else
+        cout << "not palindrom";
+
+    return 0;
+}
+
+/*
+output:------
+enter the size of first linked list : 4
+
+enter 4 numbers : 1 2 3 4
+not palindrom
+
+enter the size of first linked list : 5
+
+enter 5 numbers : 1 2 3 2 1
+palindrom
+*/
