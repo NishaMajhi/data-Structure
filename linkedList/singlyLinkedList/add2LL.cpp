cb78d4f5b1baa8dbe8d4ce6d0d7a410cffc2e342
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-14 17:41:25 +0530

    Update add2LL.cpp

--- linkedList/singlyLinkedList/add2LL.cpp
+++ linkedList/singlyLinkedList/add2LL.cpp
@@ -1,3 +1,89 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+Example 1:
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+
+Explanation: 342 + 465 = 807.
+
+
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+*/
+
+//leetcode solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode *ans = new ListNode(1);
+        ListNode *temp = new ListNode(1);
+        ans = temp;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL || carry != 0)
+        {
+            int value1 = 0;
+            if (l1 != NULL)
+               value1 = l1->val;
+
+            int value2 = 0;
+            if (l2 != NULL)
+               value2 = l2->val;
+
+            int sum = carry + value1 + value2;
+            int digit = sum % 10;
+    
+             ListNode *t = new ListNode(digit);
+             temp->next = t;
+             temp = t;
+            
+             carry = sum / 10;
+             if (l1 != NULL)
+                l1 = l1->next;
+
+             if (l2 != NULL)
+                l2 = l2->next;
+       }
+        return ans->next;
+    }
+};
+
+
+
+
 // add two linked list and give the answer in a form ofLinked List
 #include <iostream>
 using namespace std;
