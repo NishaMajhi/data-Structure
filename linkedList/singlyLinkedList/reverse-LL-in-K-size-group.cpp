d6d17d6e396506f013dc01e4c84071561ba52e80
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-15 15:59:01 +0530

    Update reverse-LL-in-K-size-group.cpp

--- linkedList/singlyLinkedList/reverse-LL-in-K-size-group.cpp
+++ linkedList/singlyLinkedList/reverse-LL-in-K-size-group.cpp
@@ -1,4 +1,83 @@
-// reverse a Lnked List in K size group
+/*
+
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list.
+If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+ 
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+
+Example 2:
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 5000
+0 <= Node.val <= 1000
+*/
+
+
+//iterative approach
+class Solution
+{
+public:
+    static int lengthOfLinkedList(ListNode *head)
+    {
+
+        int length = 0;
+        while (head != NULL)
+        {
+            ++length;
+            head = head->next;
+        }
+        return length;
+    }
+
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        int length = lengthOfLinkedList(head);
+
+        ListNode *dummyHead = new ListNode(0);
+        dummyHead->next = head;
+
+        ListNode *pre = dummyHead;
+        ListNode *cur;
+        ListNode *nex;
+
+        while (length >= k)
+        {
+            cur = pre->next;
+            nex = cur->next;
+            for (int i = 1; i < k; i++)
+            {
+                cur->next = nex->next;
+                nex->next = pre->next;
+                pre->next = nex;
+                nex = cur->next;
+            }
+            pre = cur;
+            length -= k;
+        }
+        return dummyHead->next;
+    }
+};
+
+
 #include <iostream>
 using namespace std;
 class Node
@@ -42,6 +121,7 @@ void push(Node *&head, Node *&tail, int d)
     }
 }
 
+//recursive approach
 Node *reverseKsize(Node *head, int k)
 {
     if (head == NULL)
