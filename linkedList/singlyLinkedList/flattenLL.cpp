f511de9122bf49055d8abc6d344bb252e6ed3700
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-19 18:51:27 +0530

    Update flattenLL.cpp

--- linkedList/singlyLinkedList/flattenLL.cpp
+++ linkedList/singlyLinkedList/flattenLL.cpp
@@ -1,52 +1,84 @@
-//Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
-// (i) a next pointer to the next node,
-// (ii) a bottom pointer to a linked list where this node is head.
-// Each of the sub-linked-list is in sorted order.
-// Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
+/*
+Given a Linked List of size N, where every node represents a sub-linked-list and contains two pointers:
+(i) a next pointer to the next node,
+(ii) a bottom pointer to a linked list where this node is head.
 
-#include <iostream>
+Each of the sub-linked-list is in sorted order.
+Flatten the Link List such that all the nodes appear in a single level while maintaining the sorted order. 
+
+Note: The flattened list will be printed using the bottom pointer instead of next pointer.
+
+Example 1:
+
+Input:
+5 -> 10 -> 19 -> 28
+|     |     |     | 
+7     20    22   35
+|           |     | 
+8          50    40
+|                 | 
+30               45
+
+Output:  5-> 7-> 8- > 10 -> 19-> 20->
+22-> 28-> 30-> 35-> 40-> 45-> 50.
+
+Explanation:
+The resultant linked lists has every 
+node in a single level.
+(Note: | represents the bottom pointer.)
+ 
+
+
+Example 2:
+
+Input:
+5 -> 10 -> 19 -> 28
+|          |                
+7          22   
+|          |                 
+8          50 
+|                           
+30              
+
+Output: 5->7->8->10->19->22->28->30->50
+
+Explanation:
+The resultant linked lists has every
+node in a single level.
+
+(Note: | represents the bottom pointer.)
+ 
+
+Your Task:
+You do not need to read input or print anything. Complete the function flatten() that takes the head of the linked list as input
+parameter and returns the head of flattened link list.
+
+ 
+
+Expected Time Complexity: O(N*N*M)
+Expected Auxiliary Space: O(1)
+
+ 
+*/
+#include <bits/stdc++.h>
 using namespace std;
-class Node
-{
-public:
-    int data;
-    Node *next;
-    Node *bottom;
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
 };
 
-Node *create(int d)
-{
-    Node *newNode = new Node();
-    newNode->data = d;
-    newNode->next = newNode->bottom = NULL;
-    return newNode;
-}
+/*  Function which returns the  root of 
+    the flattened linked list. */
 
-void print(Node *&head)
-{
-    cout << "data of linked list : ";
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-    cout << endl;
-}
-
-void push(Node *&head, Node *&tail, int d)
-{
-    if (head == NULL)
-    {
-        head = create(d);
-        tail = head;
-    }
-    else
-    {
-        tail->next = create(d);
-        tail = tail->next;
-    }
-}
 Node *merge(Node *d,Node *r)
 {
     if(d == NULL)   
@@ -55,8 +87,13 @@ Node *merge(Node *d,Node *r)
     if(r == NULL)
          return d;
       
+    // if(d->bottom == NULL)
+    // {
+    //     d->bottom = r;
+    //     return d;
+    // }
     
-    Node *res = create(1);
+    Node *res = new Node(1);
     Node *temp = res;
     
     while(d != NULL && r!= NULL)
@@ -102,26 +139,79 @@ Node *flatten(Node *root)
     
     return res;
     
-   
+  
 }
 
-int main()
+void printList(Node *Node)
 {
-    Node *head, *tail;
-    head = tail = NULL;
-
-    // creating initial nodes in Linked List
-    int n;
-    cout << "enter the size of linked list : ";
-    cin >> n;
-    cout << endl
-         << "enter " << n << " numbers : ";
-    while (n--)
+    while (Node != NULL)
     {
-        int d;
-        cin >> d;
-        push(head, tail, d);
+        printf("%d ", Node->data);
+        Node = Node->bottom;
     }
-    head = flatten(head);
-    print(head);
 }
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+// } Driver Code Ends
+
