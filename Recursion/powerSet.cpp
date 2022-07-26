2df7cb38d180878d9b593c71448e6efd1b6fe4de
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-26 12:48:08 +0530

    Add files via upload

---
+++ Recursion/powerSet.cpp
@@ -0,0 +1,56 @@
+// find all the set of input array;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void powerSet(vector<int> &arr, vector<vector<int>> &ans, vector<int> temp, int index, int &count1)
+{
+    // if we reach end of the input array
+    if (index >= arr.size())
+    {
+        ans.push_back(temp);
+        count1++;
+        return;
+    }
+
+   
+
+    // excluding
+    powerSet(arr, ans, temp, index + 1, count1);
+
+     // including index element in the temporary vector
+    temp.push_back(arr[index]);
+    powerSet(arr, ans, temp, index + 1, count1);
+}
+
+int main()
+{
+    int n, count1 = 0;
+    cout << "enter size : ";
+    cin >> n;
+    // create input array and a temp vector for storing result of every subset
+    vector<int> arr(n), temp;
+
+    // for storing all subsets
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // call function
+    powerSet(arr, ans, temp, 0, count1);
+
+    // print all subsets
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // print total count of subset
+    cout << "count of total subset : " << count1 << endl;
+}
