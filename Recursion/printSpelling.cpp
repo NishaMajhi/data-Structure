9c36867677d3b713d83ee2728cafc6fc5992c632
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-07-26 11:42:27 +0530

    Add files via upload

---
+++ Recursion/printSpelling.cpp
@@ -0,0 +1,40 @@
+// print spelling of given number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSpell(int n, unordered_map<int, string> mp)
+{
+    // base condition
+    if (n == 0)
+        return;
+
+    // recursive call
+    printSpell(n / 10, mp);
+
+    // processing/printing
+    cout << mp[n % 10] << " ";
+}
+
+int main()
+{
+    int n;
+    cout << "enter number : ";
+    cin >> n;
+    // map number with their spelling
+    unordered_map<int, string> mp;
+    mp[0] = "zero";
+    mp[1] = "one";
+    mp[2] = "two";
+    mp[3] = "three";
+    mp[4] = "four";
+    mp[5] = "five";
+    mp[6] = "six";
+    mp[7] = "seven";
+    mp[8] = "eight";
+    mp[9] = "nine";
+
+    printSpell(n, mp);
+
+    return 0;
+}
