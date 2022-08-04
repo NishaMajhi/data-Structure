fe89bfbb19fc0798943973843f3dc493da1c26c3
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-08-04 11:47:42 +0530

    Update Binary Tree level Order Traversal Iterative Approach.cpp

--- Tree/codingQuestion/Binary Tree level Order Traversal Iterative Approach.cpp
+++ Tree/codingQuestion/Binary Tree level Order Traversal Iterative Approach.cpp
@@ -23,6 +23,9 @@ The number of nodes in the tree is in the range [0, 2000].
 
 */
 
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -34,6 +37,63 @@ The number of nodes in the tree is in the range [0, 2000].
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//approach 1
+class Solution {
+public:
+       
+    vector<vector<int>> levelOrder(TreeNode* root) {
+               
+        //create resultant vector
+        vector<vector<int>> ans;
+        if(root == NULL)
+            return ans;
+        
+        //create a Queue for storing node
+        queue<TreeNode*> q;
+        
+        //push the first node along with NULL value because 1sr level is complete
+        q.push(root);
+        q.push(NULL);
+        
+        //create a temporary vector to store data of each levels
+        vector<int> v;
+        
+        while(!q.empty()){
+            
+            TreeNode *t = q.front();
+            q.pop();
+            
+            //if current node is NULL means a level is completed
+            if(t == NULL){
+                ans.push_back(v);
+                
+                //if queue still has some nodes after completing a level
+                if(!q.empty()){
+                   q.push(NULL);
+                }
+               //clear the previous data from vector in order to store new values of new level
+                v.clear();
+            }
+            
+            else{
+                v.push_back(t->val);
+                
+                if(t->left)
+                    q.push(t->left);
+                if(t->right)
+                    q.push(t->right);
+            }
+            
+        }
+        
+        return ans;
+    }
+};
+
+
+
+//approach 2
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
