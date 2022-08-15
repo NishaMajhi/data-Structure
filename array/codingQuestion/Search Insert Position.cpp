a7abba55bb0a11a3f5f3a4bcde34de05b287967d
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-08-15 22:01:49 +0530

    Update Search Insert Position.cpp

--- array/codingQuestion/Search Insert Position.cpp
+++ array/codingQuestion/Search Insert Position.cpp
@@ -4,7 +4,6 @@ Given a sorted array of distinct integers and a target value, return the index i
 If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
-
  
 
 Example 1:
