d37b71c21121a5b2a90fdca177ccd9e7debea4d9
Nisha Majhi <84006994+NishaMajhi@users.noreply.github.com>
2022-12-05 08:38:25 +0530

    Create Partition List.cpp

---
+++ linkedList/singlyLinkedList/Partition List.cpp
@@ -0,0 +1,64 @@
+/*
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+ 
+Example 1:
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+
+
+Example 2:
+Input: head = [2,1], x = 2
+Output: [1,2]
+ 
+
+Constraints:
+The number of nodes in the list is in the range [0, 200].
+-100 <= Node.val <= 100
+-200 <= x <= 200
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        
+        ListNode *lessVal = new ListNode(-1);
+        ListNode *lessTail = lessVal;
+        ListNode *curr  = head;
+        ListNode *greatVal = new ListNode(-1);
+        ListNode *greatTail = greatVal;
+        
+        while(curr != NULL){
+            if(curr->val < x){
+                lessTail->next = curr;
+                lessTail = curr;
+            }
+            else{
+                greatTail->next = curr;
+                greatTail = curr;
+            }
+            
+            curr = curr->next;
+        }
+        
+        greatTail->next = NULL;
+        lessTail->next = greatVal->next;
+        
+        head = lessVal->next;
+        
+        return head;
+    }
+};
